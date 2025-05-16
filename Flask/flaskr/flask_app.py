59ecf715aac49e2fcba742de3d11ca8dce7921fa
SUPERNOOB20 <andycia802@gmail.com>
2025-05-16 01:32:07 -0300

    slamming my head trying to make the backend work :)))))

--- Flask/flaskr/flask_app.py
+++ Flask/flaskr/flask_app.py
@@ -42,9 +42,15 @@ def hyperlink_to_archived_posts(name=None):
     # render_template("new_blog_posts.html", person=name)
     return render_template("archives_button.html", click = move_to)
 
+
 @app.route("/archive")
 def archived_posts(name=None):
     # post = <blockquote class="twitter-tweet"><p lang="en" dir="ltr">Programming monkey discovers Python JSON parsing after having done her whole saving system manually and scattered out in a txt file 🤦‍♀️ <a href="https://t.co/wTAomfY2eU">pic.twitter.com/wTAomfY2eU</a></p>&mdash; SUPERNOOB20 (@SUPERNOOB_20) <a href="https://twitter.com/SUPERNOOB_20/status/1920927904885596524?ref_src=twsrc%5Etfw">May 9, 2025</a></blockquote> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
     # return {% include 'post_template' %}
     # return post
     return render_template("list_of_archives.html", person=name)
+
+
+@app.route("/portfolio")
+def portfolio(name=None):
+    return render_template("portfolio.html", person=name)
